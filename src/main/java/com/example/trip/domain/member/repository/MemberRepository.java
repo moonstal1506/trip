58beff88270a26d8f3f7ea83993f5052c9462333
moonstal1506 <moonstal1506@gmail.com>
2023-12-29 18:15:44 +0900

    feat: 중복 회원 예외 처리

--- src/main/java/com/example/trip/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/trip/domain/member/repository/MemberRepository.java
@@ -4,4 +4,5 @@ import com.example.trip.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByName(String name);
 }
--- src/main/java/com/example/trip/domain/member/service/MemberService.java
+++ src/main/java/com/example/trip/domain/member/service/MemberService.java
@@ -13,7 +13,15 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public void createMember(MemberCreateRequestDto memberCreateRequestDto) {
+        validateMember(memberCreateRequestDto);
         Member member = memberCreateRequestDto.createMember();
         memberRepository.save(member);
+    }
+
+    private void validateMember(MemberCreateRequestDto memberCreateRequestDto) {
+        Member findMember = memberRepository.findByName(memberCreateRequestDto.getName());
+        if (findMember != null) {
+            throw new IllegalStateException("중복된 이름입니다.");
+        }
     }
 }
