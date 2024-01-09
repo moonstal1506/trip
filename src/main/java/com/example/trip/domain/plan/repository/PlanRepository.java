73ebc0baebb90b32acfb2c73031d46a7bf906e4f
moonstal1506 <moonstal1506@gmail.com>
2024-01-10 04:09:54 +0900

    refactor: 여행 계획 조회 N+1 해결

--- src/main/java/com/example/trip/domain/plan/repository/PlanRepository.java
+++ src/main/java/com/example/trip/domain/plan/repository/PlanRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlanRepository extends JpaRepository<Plan, Long> {
 
     // JPQL
@@ -15,4 +17,7 @@ public interface PlanRepository extends JpaRepository<Plan, Long> {
     // EntityGraph
     @EntityGraph(attributePaths = "member")
     Page<Plan> findAll(Pageable pageable);
+
+    @EntityGraph(attributePaths = "planAttractions.attraction")
+    Optional<Plan> findById(Long planId);
 }
